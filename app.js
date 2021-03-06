363e55882daf980afee0a9656c88ff9da94ee93e
Friday Okorie <fridayokorieatgit.com@gmail.com>
2021-03-06 05:55:10 +0100

    Preliminary Setup, can now get city name with all-the-cities npm package

--- app.js
+++ app.js
@@ -1,60 +1,50 @@
-const express = require('express');
-const _ = require('lodash');
-const bodyParser = require('body-parser');
-const random = require('random-world');
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const randomCities = require("all-the-cities");
+const { static, response } = require("express");
+const { indexOf } = require("lodash");
+
+//Components
 const cities = [];
-const app = express();
+const cardComponents = [];
+
+//Globals
+const apiKey = "8aeb0b8987a14d50b54e3d719f94b3cf";
 const port = 3000;
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs')
-let randomCity; 
-app.get('/', (req, res) => {
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(static('public'))
+
+app.get("/", (req, res) => {
   for (let i = 0; i < 8; i++) {
-    randomCity = random.city();
-    cities[i] = randomCity;
+    const randomIndex = Math.floor(Math.random() * randomCities.length);
+    cities[i] = _.upperCase(randomCities[randomIndex].name);
   }
 
-  console.log(cities);
-  res.render('home', {
+  cities.forEach(city => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    https.get(url, response =>{
+      response.on('data', data => {
+        let weatherData = JSON.parse(data);
+        console.log(city);
+        cardComponents[indexOf(city)] = weatherData.weather[0];
+        console.log(cardComponents);
+      });
+    });
+  });
+
+  res.render("home", {
     cities: cities,
   });
 });
 
-
-app.listen(port, ()=> {
-  console.log(`Server running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server open and listening at port ${port}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //api.openweathermap.org/data/2.5/weather?q=Abuja&appid=8aeb0b8987a14d50b54e3d719f94b3cf&units=metric
 
