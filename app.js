8210af606d1d39989d1d640cced2176dc3378d57
Friday Okorie <fridayokorieatgit.com@gmail.com>
2021-03-07 11:36:25 +0100

    ask for help

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const bodyParser = require("body-parser");
 const https = require("https");
 const randomCities = require("all-the-cities");
 const { static, response } = require("express");
-const { indexOf, lastIndexOf } = require("lodash");
+const { indexOf, lastIndexOf, findIndex } = require("lodash");
 
 //Components
-const cities = [];
-const cardComponents = [];
+let cities = ['New York', 'Lagos', 'Paris', 'Los Angeles', 'Havana', 'Moscow', 'Tokyo', 'Dubai', 'Abuja', 'Barcelona', 'Singapore', 'Medellin'];
+let cardComponents = [];
 
 //Globals
 const apiKey = "8aeb0b8987a14d50b54e3d719f94b3cf";
@@ -20,29 +20,41 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(static("public"));
 
 app.get("/", (req, res) => {
-  for (let i = 0; i < 8; i++) {
-    const randomIndex = Math.floor(Math.random() * randomCities.length);
-    cities[i] = _.upperCase(randomCities[randomIndex].name);
-  }
+  // for (let i = 0; i < 8; i++) {
+  //   const randomIndex = Math.floor(Math.random() * randomCities.length);
+  //   cities[i] = _.upperCase(randomCities[randomIndex].name);
+  // }
 
-  cities.forEach((city) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  // // for (let i = 0; i < cities.length; i++) {
+  // //   let city = cities[i];
+  // //    let url = `https://api.openweather.org/data/2.5/weather?q=Abuja&appid=${apiKey}&units=metric`;
+  // //    https.get(url, response => {
+  // //      response.on('data', data => {
+  // //        let weatherData = JSON.parse(data);
+  // //        cardComponents[i] = response.statusCode;
+  // //      });
+  // //    });
+  // // };
 
+  cities.forEach((city, cityIndex) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${_.lowerCase(city)}&appid=${apiKey}&units=metric`;
     https.get(url, (response) => {
-      let status = response.statusCode;
       response.on("data", (data) => {
         let weatherData = JSON.parse(data);
-        // let cityWeather = weatherData.weather[0]
-        cardComponents.push(status)
-        console.log(cardComponents);
+        let weather = weatherData.weather[0];
+        cardComponents[cityIndex] = weather;
       });
     });
   });
+
+
+
 
   // console.log(cardComponents);
 
   res.render("home", {
     cities: cities,
+    cardComponents: cardComponents,
   });
 });
 
